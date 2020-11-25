cdf49912175c456f22c36915af455374d9d70420
Saed Daoud <eng.saed.daoud@gmail.com>
2020-11-24 21:04:58 -0500

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -14,6 +15,14 @@ connectDB();
 
 // routes
 app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
 
 // Error Middlewares
 app.use(notFound);
