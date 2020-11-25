8acb5f306dcd58d96411cde6e08750a92ad4ce43
Saed Daoud <eng.saed.daoud@gmail.com>
2020-11-24 21:41:32 -0500

    deploy 3

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -31,7 +31,7 @@ const CartScreen = () => {
 
   return (
     <motion.div
-      className='page product-page'
+      className='page cart-page'
       exit={{ opacity: 0 }}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
